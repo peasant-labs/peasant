310f242152280757c31f021528743622fe3c243f
David Huu Pham <dayvidpham@gmail.com>
2026-08-13 21:38:55 -0700

    build: refresh nix vendor hash

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
       # Vendor hash for buildGoModule. Recompute when go.mod/go.sum changes:
       # set this to nixpkgs.lib.fakeHash, run `nix build`, copy the reported `got:`
       # hash back here.
-      vendorHash = "sha256-8Nu0N9h/KZrl9pJBNUiFfCYPlkpo61fNvTqnlcr0H34=";
+      vendorHash = "sha256-wkUPgRf8s+onWaVng8WTK42DrCz1iyMzmkqY7FeP6gk=";
 
       # Extra CLI tools available in the dev shell
       devTools = pkgs: with pkgs; [
